ec13f31fc43a329bf4bd9c7d5fd71a2d1cb5a099
pparonson <preston.aronson@gmail.com>
2018-02-26 07:50:29 -0700

    karma tdd config and create initial block and blockchain fns

--- src/sum/sum.test.js
+++ src/sum/sum.test.js
@@ -1,12 +1,19 @@
 const expect = require("expect");
 
-//import * as sum from "./sum";
+import * as sum from "./sum";
 // const add = require("./sum.js");
 
 describe("blockchain_fnjs", () => {
   describe("testing environment status", () => {
     it("should properly run tests", () => {
       expect(1).toBe(1);
+    });
+  });
+
+  describe("sum", () => {
+    it("should return 3", () => {
+      let result = sum.add(1, 2);
+      expect(result).toBe(3);
     });
   });
 
